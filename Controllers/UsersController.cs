3eb988a45672f69d6be1bb939d40f5925f6cfb16
Rostyslav Ostapiuk <rostyslav.ostapiuk@gmail.com>
2019-04-27 23:12:06 +0200

    add user controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _datingRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IDatingRepository datingRepository, IMapper mapper)
+        {
+            
+            _datingRepository = datingRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _datingRepository.GetUsers();
+
+            var usersForList = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok(usersForList);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _datingRepository.GetUser(id);
+
+            UserForDetailedDto mappedUser = _mapper.Map<UserForDetailedDto>(user);
+
+            return Ok(mappedUser);
+        }
+    }
+}
